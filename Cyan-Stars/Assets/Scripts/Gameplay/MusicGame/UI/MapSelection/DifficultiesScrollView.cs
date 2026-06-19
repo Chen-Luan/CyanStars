1a68f78b73592d360a059f4cf78afddb3f60d469
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-06-20 02:57:54 +0800

    Squashed commit of the following:
    commit 02ec086c3bbecda4759e67a47019d6a7abd3f0d9
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Sat Jun 20 02:55:10 2026 +0800
    
        Fix: 更新资源，修复漏洞
    
    commit 67f4c020832c15989c0d49196994dde873bf34e7
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Fri Jun 19 02:08:53 2026 +0800
    
        Update: 更新内置谱包 SO 结构，移除运行时谱包难度检测
    
    commit 174a2ed5d6493f2ec0442bfc9a4c692633d8bb61
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Fri Jun 19 02:03:34 2026 +0800
    
        Del: 移除 MusicGameTrackModule，直接改用 TrackLoaderRegistry
    
    commit 0de6fa9a3a5efe0fe0954de9c074b742d7da3cd5
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Fri Jun 19 01:57:23 2026 +0800
    
        [AIGC]Refactor: 重构 ChartModule
    
    commit 2c47bd6e80dd93b61c38cbd6961ed497e98bfc3e
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Fri Jun 19 00:25:06 2026 +0800
    
        Update: 更新谱面加载逻辑
    
    commit 94c21b31c63715e4cfd27c8b6a5fb9f88c35909c
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Thu Jun 18 17:31:14 2026 +0800
    
        Squashed commit of the following:
    
        commit 0723575cb9d1e659faf5ced33e157d39b3d00316
        Author: CL <62653664+Chen-Luan@users.noreply.github.com>
        Date:   Tue Jun 9 02:27:28 2026 +0800
    
            Fix: typo
    
        commit 5935873a4ea2625782087791680de4baa65f0444
        Author: CL <62653664+Chen-Luan@users.noreply.github.com>
        Date:   Tue Jun 9 02:17:04 2026 +0800
    
            Enhance: 修复内存泄漏
    
        commit 98f5f1c72a76570791a51dbc2b7f09bc2bd7fbb1
        Author: CL <62653664+Chen-Luan@users.noreply.github.com>
        Date:   Tue Jun 9 02:16:04 2026 +0800
    
            Del: 移除未使用的哈希、按难度选择谱面
    
        commit 4c13c4914e098a14760e29beb21074ce87a2d266
        Author: CL <62653664+Chen-Luan@users.noreply.github.com>
        Date:   Tue Jun 9 01:59:31 2026 +0800
    
            Refactor: 分离谱包加载和 ChartModule 逻辑*
    
            AI 编写，人工 CR
    
        commit f587e5cf39c6d471abcd97a10be4feacb8cc694b
        Author: CL <62653664+Chen-Luan@users.noreply.github.com>
        Date:   Tue Jun 9 01:21:50 2026 +0800
    
            Update: 更新谱包加载方式和注释，使其更易读
    
    commit 216921fcb9b72519f5a9a103e0de1276ee8c3160
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Tue Jun 16 23:17:28 2026 +0800
    
        Update CysMultimediaAssets
    
    commit 3fbf66d89a47db28f84bf429a5fb783cbb21ad07
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Tue Jun 16 23:05:50 2026 +0800
    
        Update: 增强异步竟态防护，提高性能
    
    commit 71c01a491192a248c73e1dc20cf4267fa937d695
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Tue Jun 16 22:50:00 2026 +0800
    
        [WIP] Feat: 部分实现难度按钮更新逻辑
    
    commit 764dfb85d6b8a6b5f7816a80c4baacf074513724
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Tue Jun 16 19:32:04 2026 +0800
    
        Del: 移除 Level 相关

---
+++ Cyan-Stars/Assets/Scripts/Gameplay/MusicGame/UI/MapSelection/DifficultiesScrollView.cs
@@ -0,0 +1,216 @@
+#nullable enable
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CyanStars.Chart;
+using CyanStars.Framework;
+using CyanStars.Framework.UI;
+using CyanStars.Utils.RadioButton;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace CyanStars.Gameplay.MusicGame
+{
+    [RequireComponent(typeof(ScrollRect))]
+    public class DifficultiesScrollView : UIBehaviour
+    {
+        [SerializeField]
+        private ScrollRect scrollRect = null!;
+
+        [SerializeField]
+        private RectTransform scrollContentRect = null!;
+
+        [SerializeField]
+        private VerticalLayoutGroup verticalLayoutGroup = null!;
+
+        [SerializeField]
+        private RadioButtonGroup radioButtonGroup = null!;
+
+        [SerializeField]
+        private GameObject difficultyItemTemplate = null!;
+
+
+        private ChartModule chartModule = null!;
+
+        private RuntimeChartPack? lastRuntimeChartPack = null; // 上次选中的谱包
+        private int laseSelectChartIndex = -1; // 上次选中的谱面下标
+        private CancellationTokenSource? cts;
+        private int validDifficultyCounts = 0; // 当前谱包内可游玩的谱面计数
+        private static UIManager UIManager => GameRoot.UI;
+        private readonly List<DifficultyItemTemplate> DifficultyItems = new();
+
+
+        protected override void Start()
+        {
+            base.Start();
+            chartModule = GameRoot.GetDataModule<ChartModule>();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            chartModule.OnSelectedChartChanged += SetDifficultiesAsync;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            chartModule.OnSelectedChartChanged -= SetDifficultiesAsync;
+        }
+
+        protected override void OnRectTransformDimensionsChange()
+        {
+            base.OnRectTransformDimensionsChange();
+
+            if (scrollRect == null || scrollContentRect == null || verticalLayoutGroup == null || validDifficultyCounts <= 0)
+                return;
+
+            UpdateContentHeight(validDifficultyCounts);
+            RefreshButtonsHeight();
+        }
+
+
+        /// <summary>
+        /// 清除旧难度按钮列表并生成新难度列表，可以一并指定选中哪个谱面
+        /// </summary>
+        /// <param name="runtimeChartPack">运行时谱包</param>
+        /// <param name="selectChartIndex">要选中的谱面下标</param>
+        /// <exception cref="InvalidDataException">没有可游玩难度</exception>
+        private async void SetDifficultiesAsync(RuntimeChartPack? runtimeChartPack, int selectChartIndex)
+        {
+            if (runtimeChartPack == lastRuntimeChartPack && selectChartIndex == laseSelectChartIndex)
+                return;
+
+            // 卸载旧谱面
+            cts?.Cancel();
+            cts?.Dispose();
+            cts = new CancellationTokenSource();
+
+            var metaDatas = runtimeChartPack?.ChartPackData.ChartMetaDatas;
+            var validMetaDatas = metaDatas?.Where(metaData => metaData.Difficulty != null).ToList();
+            validDifficultyCounts = validMetaDatas?.Count ?? 0;
+
+            // 更新 scrollContent 总高度
+            UpdateContentHeight(validDifficultyCounts);
+
+            // 释放旧难度按钮
+            List<BaseUIItem> baseItems = new();
+            baseItems.AddRange(DifficultyItems);
+            UIManager.ReleaseUIItems(baseItems);
+            DifficultyItems.Clear();
+
+            // 检查谱包是否有效，更新缓存变量
+            if (runtimeChartPack == null || metaDatas == null || validMetaDatas == null)
+            {
+                lastRuntimeChartPack = null;
+                laseSelectChartIndex = -1;
+                return;
+            }
+            else if (validDifficultyCounts == 0)
+            {
+                lastRuntimeChartPack = runtimeChartPack;
+                laseSelectChartIndex = -1;
+                return;
+            }
+            else
+            {
+                lastRuntimeChartPack = runtimeChartPack;
+                laseSelectChartIndex = selectChartIndex;
+            }
+
+            // 取回新难度按钮，注入参数，设置高度和横坐标
+            List<Task<DifficultyItemTemplate>> tasks = new();
+            for (int i = 0; i < validDifficultyCounts; i++)
+            {
+                var valueTask = UIManager.GetUIItemAsync<DifficultyItemTemplate>(difficultyItemTemplate, scrollContentRect);
+                tasks.Add(valueTask.AsTask());
+            }
+
+            await Task.WhenAll(tasks);
+            if (cts.Token.IsCancellationRequested || this == null || gameObject == null)
+            {
+                // 如果被取消，释放刚刚获取的 UI Items 防止内存泄露
+                List<BaseUIItem> tempItems = new();
+                foreach (var task in tasks)
+                    if (task.Status == TaskStatus.RanToCompletion && task.Result != null)
+                        tempItems.Add(task.Result);
+
+                UIManager.ReleaseUIItems(tempItems);
+                return;
+            }
+
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                var difficultyItem = tasks[i].Result;
+                var metadata = validMetaDatas[i];
+                // TODO: 内置谱 text 改为难度+定数（国际化字段），玩家谱包 text 改为谱面标题
+                difficultyItem.Init(radioButtonGroup, metadata.Difficulty!.Value, i == selectChartIndex, "TODO");
+                DifficultyItems.Add(difficultyItem);
+            }
+
+            RefreshButtonsHeight();
+            // TODO: 初始化完毕后更新一次物体横坐标，当滚动 ScrollRect 时也要每帧更新。
+        }
+
+
+        /// <summary>
+        /// 更新 Content 高度
+        /// </summary>
+        private void UpdateContentHeight(int difficultyCounts)
+        {
+            switch (difficultyCounts)
+            {
+                case <= 0:
+                    throw new InvalidDataException("谱包不存在任何可游玩难度，无法更新难度。");
+                case <= 4:
+                {
+                    float height = ((RectTransform)scrollRect.transform).rect.height;
+                    scrollContentRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+                    break;
+                }
+                default:
+                {
+                    float buttonHeight = CalculateButtonHeight();
+
+                    float contentHeight = 0;
+                    contentHeight += verticalLayoutGroup.padding.top;
+                    contentHeight += verticalLayoutGroup.padding.bottom;
+                    contentHeight += difficultyCounts * buttonHeight;
+                    contentHeight += (difficultyCounts - 1) * verticalLayoutGroup.spacing;
+                    scrollContentRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, contentHeight);
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计算单个按钮高度，确保 4 个按钮高度 + 3 个间距 + 上下 padding 正好填满一页
+        /// </summary>
+        /// <returns>单个按钮高度</returns>
+        private float CalculateButtonHeight()
+        {
+            float scrollHeight = ((RectTransform)scrollRect.transform).rect.height;
+            float buttonHeight = ((scrollHeight - verticalLayoutGroup.padding.top - verticalLayoutGroup.padding.bottom) -
+                                  (verticalLayoutGroup.spacing * 3))
+                                 / 4;
+            return buttonHeight;
+        }
+
+        /// <summary>
+        /// 在设置完难度或 Canvas 高度变化时，更新所有难度按钮的高度
+        /// </summary>
+        private void RefreshButtonsHeight()
+        {
+            var height = CalculateButtonHeight();
+
+            foreach (var item in DifficultyItems)
+                item.SetRectTransformHeight(height);
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(scrollContentRect);
+        }
+    }
+}
